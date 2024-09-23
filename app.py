abcee90aa988e0abbdce52d46ab91bb52c31aa8d
pedrozau <pedrozau28@gmail.com>
2024-09-23 16:17:00 +0200

    improving the web app vuln

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
         {"name": "DVWA", "port": 8081},
         {"name": "Juice Shop", "port": 8082},
         {"name": "Mutillidae", "port": 8083},
-        {"name": "WebGoat", "port": 8084},
+        {"name": "WebGoat", "port": '8084/WebGoat/'},
         {"name": "bWAPP", "port": 8085},
         {"name": "Hackazon", "port": 8086},
         {"name": "XVWA", "port": 8087},
@@ -17,7 +17,7 @@ def index():
 
     # Gera as URLs dinamicamente com base no host atual
     for app in apps:
-        app["url"] = f"{request.host_url.rstrip('/')}/{app['port']}"
+        app["url"] = f"{request.host_url.rstrip('/')}:{app['port']}"
 
     return render_template('index.html', apps=apps)
 
